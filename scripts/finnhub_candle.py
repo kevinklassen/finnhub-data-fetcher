005eb27b9753f48e084f8e5a8ef4597298970bce
Kevin Klassen <kevink@hey.com>
2023-10-09 23:10:48 -0400

    shrink scripts to work with new  module

--- scripts/finnhub_candle.py
+++ scripts/finnhub_candle.py
@@ -1,61 +1,24 @@
-# Description: Fetches candle data for a list of tickers from the Finnhub API
-# and saves the data to a CSV file.
-import asyncio
 from datetime import datetime, date, timedelta
-import os
-import pandas as pd
-import random
 from modules.fetch_finnhub import fetch_finnhub_data
-from modules.plot_api_calls import plot_api_calls_per_minute, plot_api_calls_per_second
 
-# Constants
-SIMULTANEOUS_CONNECTIONS = 2
-API_DELAY = 6 / 7
+# Parameters
+ENDPOINT = "candle"
+SIMULTANEOUS_CONNECTIONS = 1
+API_DELAY = 1
 QUERY_MAX = 5
+RESOLUTION = "D"
 START_DATE = int(
     datetime.strptime(str(date.today() - timedelta(days=7)), "%Y-%m-%d").timestamp()
 )
 END_DATE = int(datetime.strptime(str(date.today()), "%Y-%m-%d").timestamp())
-output_folder = "test/data/python"
 
-# Load the Finnhub investable universe
-finnhub_universe_df = pd.read_csv("test/data/python/finnhub_investable_universe.csv")
-tickers = finnhub_universe_df["Ticker"].tolist()
-
-# Randomize the order of the tickers
-random.shuffle(tickers)
-tickers = tickers[1:5]
-
-# Fetch the candle data
-results_df, api_call_timestamps = asyncio.run(
-    fetch_finnhub_data(
-        tickers,
-        endpoint="candle",
-        resolution="D",
-        start_date=START_DATE,
-        end_date=END_DATE,
-        simultaneous_connections=SIMULTANEOUS_CONNECTIONS,
-        api_delay=API_DELAY,
-        query_max=QUERY_MAX,
-    )
+# Fetch from Finnhub API
+results_df, api_call_timestamps = fetch_finnhub_data(
+    endpoint=ENDPOINT,
+    simultaneous_connections=SIMULTANEOUS_CONNECTIONS,
+    api_delay=API_DELAY,
+    query_max=QUERY_MAX,
+    resolution=RESOLUTION,
+    start_date=START_DATE,
+    end_date=END_DATE,
 )
-
-# Save Results to CSV
-results_df.to_csv(os.path.join(output_folder, "finnhub_candle.csv"), index=False)
-
-# Convert results_df t column to datetime
-results_df["t"] = pd.to_datetime(results_df["t"], unit="s")
-
-# Save API Call Timestamps to CSV
-api_call_timestamps_df = pd.DataFrame(api_call_timestamps)
-api_call_timestamps_df.to_csv(
-    os.path.join(
-        output_folder,
-        f"finnhub_candle_api_call_timestamps_{pd.Timestamp.now().date()}.csv",
-    ),
-    index=False,
-)
-
-# Plot API Call Timestamps
-plot_api_calls_per_minute(api_call_timestamps)
-plot_api_calls_per_second(api_call_timestamps)
--- scripts/finnhub_financials_ic_a.py
+++ scripts/finnhub_financials_ic_a.py
@@ -1,53 +1,19 @@
-# Description: Fetches annual income statement data for a list of tickers from
-# the Finnhub API and saves the data to a CSV file.
-import asyncio
-import os
-import pandas as pd
-import random
 from modules.fetch_finnhub import fetch_finnhub_data
-from modules.plot_api_calls import plot_api_calls_per_minute, plot_api_calls_per_second
 
-# Constants
+# Parameters
+ENDPOINT = "financials"
 SIMULTANEOUS_CONNECTIONS = 2
 API_DELAY = 6 / 7
 QUERY_MAX = 5
-output_folder = "test/data/python"
+STATEMENT = "ic"
+FREQ = "annual"
 
-# Load the Finnhub investable universe
-finnhub_universe_df = pd.read_csv("test/data/python/finnhub_investable_universe.csv")
-tickers = finnhub_universe_df["Ticker"].tolist()
-
-# Randomize the order of the tickers
-random.shuffle(tickers)
-
-# Fetch the annual income statement data
-results_df, api_call_timestamps = asyncio.run(
-    fetch_finnhub_data(
-        tickers,
-        endpoint="financials",
-        statement="ic",
-        freq="annual",
-        simultaneous_connections=SIMULTANEOUS_CONNECTIONS,
-        api_delay=API_DELAY,
-        query_max=QUERY_MAX,
-    )
+# Fetch from Finnhub API
+results_df, api_call_timestamps = fetch_finnhub_data(
+    endpoint=ENDPOINT,
+    simultaneous_connections=SIMULTANEOUS_CONNECTIONS,
+    api_delay=API_DELAY,
+    query_max=QUERY_MAX,
+    statement=STATEMENT,
+    freq=FREQ,
 )
-
-# Save Results to CSV
-results_df.to_csv(
-    os.path.join(output_folder, "finnhub_financials_ic_a.csv"), index=False
-)
-
-# Save API Call Timestamps to CSV
-api_call_timestamps_df = pd.DataFrame(api_call_timestamps)
-api_call_timestamps_df.to_csv(
-    os.path.join(
-        output_folder,
-        f"finnhub_financials_ic_a_api_call_timestamps_{pd.Timestamp.now().date()}.csv",
-    ),
-    index=False,
-)
-
-# Plot API Call Timestamps
-plot_api_calls_per_minute(api_call_timestamps)
-plot_api_calls_per_second(api_call_timestamps)
