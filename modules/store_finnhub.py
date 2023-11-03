f55ac3e2b19c54ef3c041c5ac1bb6f2ff07a7962
Kevin Klassen <kevink@hey.com>
2023-11-02 21:59:21 -0400

    first attempt to store to S3

--- modules/store_finnhub.py
+++ modules/store_finnhub.py
@@ -3,19 +3,32 @@
 This module provides functions to store data pulled from the Finnhub API.
 """
 
+import boto3
 import jsonlines
+import io
 
 
 def write_to_jsonl(data, file_path):
     """
-    Writes data to a JSONL file.
+    Writes data to a JSONL file on S3.
 
     Parameters:
     - data (list): A list of dictionaries containing data for each ticker.
-    - file_path (str): The path where the JSONL file will be saved.
+    - file_path (str): The S3 key where the JSONL file will be saved in the
+    account-jordanmckinnie-vendor-finnhub bucket.
 
     Returns:
     - None
     """
-    with jsonlines.open(file_path, mode="w") as writer:
+
+    # Set up S3 client
+    s3 = boto3.client("s3")
+
+    # Write data to in-memory buffer
+    buffer = io.BytesIO()
+    with jsonlines.Writer(buffer) as writer:
         writer.write_all(data)
+
+    # Upload buffer to S3
+    bucket_name = "account-jordanmckinnie-vendor-finnhub"
+    s3.upload_fileobj(buffer, bucket_name, file_path)
