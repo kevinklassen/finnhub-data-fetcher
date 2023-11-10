2ae13b1391d16e64530f6fc28598a935dae20317
Kevin Klassen <kevink@hey.com>
2023-11-10 06:14:34 -0500

    update to use spark to store as jsonl file, change function name to 'store_data' to be consistent with fetch_finnhub module naming

--- modules/store_finnhub.py
+++ modules/store_finnhub.py
@@ -3,32 +3,41 @@
 This module provides functions to store data pulled from the Finnhub API.
 """
 
-import boto3
-import jsonlines
-import io
+from pyspark.sql import SparkSession
+from datetime import datetime
+
+# Initialize a Spark session
+spark = SparkSession.builder.getOrCreate()
 
 
-def write_to_jsonl(data, file_path):
+def store_data(data, file_path):
     """
-    Writes data to a JSONL file on S3.
+    Writes data to a JSONL file on S3 using spark.
 
     Parameters:
     - data (list): A list of dictionaries containing data for each ticker.
-    - file_path (str): The S3 key where the JSONL file will be saved in the
-    account-jordanmckinnie-vendor-finnhub bucket.
+    - file_path (str): The path from the account-jordanmckinnie-vendor-finnhub
+    bucket root where the JSONL file will be saved. The file name will be
+    the current date in the format YYYY_MM_DD.
 
     Returns:
     - None
     """
 
-    # Set up S3 client
-    s3 = boto3.client("s3")
+    # Convert the list of dictionaries to a Spark DataFframe
+    data_df = spark.createDataFrame(data)
+    print(data_df.show(5))
 
-    # Write data to in-memory buffer
-    buffer = io.BytesIO()
-    with jsonlines.Writer(buffer) as writer:
-        writer.write_all(data)
-
-    # Upload buffer to S3
+    # Define the S3 bucket and key for the data
     bucket_name = "account-jordanmckinnie-vendor-finnhub"
-    s3.upload_fileobj(buffer, bucket_name, file_path)
+    key = f"{file_path}/{datetime.now().strftime('%Y_%m_%d')}.jsonl"
+
+    try:
+        # Store the data in S3 using Spark
+        data_df.write.option("header", "true").mode("overwrite").json(
+            f"./{bucket_name}/{key}"
+        )
+        print(f"Successfully stored data in S3 at {key}.")
+    except Exception as e:
+        print(f"Failed to store data in S3 at {key}: {e}")
+        raise e
