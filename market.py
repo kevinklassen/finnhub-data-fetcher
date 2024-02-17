196e2ee2285c6fbd22c0c1f2076bf44625973510
Kevin Klassen <kevink@hey.com>
2024-02-17 10:07:11 -0500

    add resolution=D

--- market.py
+++ market.py
@@ -11,9 +11,5 @@ end_date = int(
 
 # Fetch from Finnhub API
 fetch_data_for_endpoint(
-    endpoint="candle",
-    start_date=start_date,
-    end_date=end_date,
-    resolution="D",
-    tickers=["AAPL"],
+    endpoint="candle", start_date=start_date, end_date=end_date, resolution="D"
 )
